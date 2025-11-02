6634928a2e35644774bdc6e7c03a697f16fd8060
nik-gamma <10471541+nikpayne@users.noreply.github.com>
2025-11-02 14:03:18 -0800

    Scroll to top hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 import Navigation from "./components/Navigation";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Piracy from "./pages/Piracy";
@@ -11,6 +12,7 @@ export const DARK_BG = "gray.900";
 function App() {
   return (
     <Box minH="100vh" bg={{ base: LIGHT_BG, _dark: DARK_BG }}>
+      <ScrollToTop />
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
